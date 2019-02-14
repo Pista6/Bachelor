f5fa23265bc6fbe0790fd8a20bb8fa45a673f23a
pista6 <happo@azet.sk>
2019-02-14 11:50:05 +0100

    Add basic routing.

--- data-analysis/src/app/app-routing.module.ts
+++ data-analysis/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnalysisContentComponent } from './analysis-content/analysis-content.component'
+import { MainContentComponent } from './main-content/main-content.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainContentComponent
+  },
+  {
+    path: 'analysis',
+    component: AnalysisContentComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
